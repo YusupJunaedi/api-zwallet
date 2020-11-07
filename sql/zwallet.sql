0bbb16cf6f97e72e238a1ab3285ad13cb8ce1dbc
YusupJunaedi <yusup.junaedi97@gmail.com>
2020-11-07 09:59:31 +0700

    change sql

--- sql/zwallet.sql
+++ sql/zwallet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2020 at 04:27 PM
+-- Generation Time: Nov 07, 2020 at 03:58 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -112,7 +112,9 @@ INSERT INTO `history` (`id_history`, `user_id`, `id_user_history`, `transaction`
 (88, 1, 26, 'Subscription', 15000, '2020-10-08 04:00:37'),
 (89, 26, 1, 'Transfer', 15000, '2020-10-08 04:00:37'),
 (90, 1, 31, 'Subscription', 15000, '2020-10-08 09:16:40'),
-(91, 31, 1, 'Transfer', 15000, '2020-10-08 09:16:40');
+(91, 31, 1, 'Transfer', 15000, '2020-10-08 09:16:40'),
+(92, 31, 29, 'Subscription', 120000, '2020-11-07 02:39:56'),
+(93, 29, 31, 'Transfer', 120000, '2020-11-07 02:39:56');
 
 -- --------------------------------------------------------
 
@@ -136,18 +138,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `username`, `email`, `password`, `pin`, `image`, `amount`, `no_hp`) VALUES
-(1, 'Yusup Junaedi', 'yusup.junaedi97@gmail.com', '$2b$10$aqvn7yjdI5mzn4FGIkjCUuRoH2uIHyYkr/e5GtslIf6V0bp1PbxzC', '632541', 'http://34.205.65.98:8000/images/1602149171752-image.jpg', 85000, '087823455567'),
-(23, 'zaki zull', 'zaki@gmail.com', '$2b$10$k14uRJOdQ8n1kTN6KNg94OffeTEDzO24NhgrIiCJwtJAn7aSCW4uC', '', 'http://34.205.65.98:8000/images/user.png', 100000, '0822888876744'),
-(24, 'Nendy Yusup', 'nendy@gmail.com', '$2b$10$U1xAoOzX/nzfAIYHQ0agMOFote6MqDLOEtxC3agGAHU1BoHKz323S', '', 'http://34.205.65.98:8000/images/user.png', 135000, '087722223455'),
-(25, 'Zaki ZUlikifli', 'zaki123@gmail.com', '$2b$10$RzILjX/4C8JLulSqmPNKe.joDEYgu5ytvgqPYaJLLNBjyy3xNZtVm', '', 'http://34.205.65.98:8000/images/user.png', 100000, '087820500512'),
-(26, 'amel', 'amel@gmail.com', '$2b$10$ACGqZjNrnUDSFbJ0LgrBl.ZW91gCNIi6.s2DgTH5uVByLUIluh1IW', '', 'http://34.205.65.98:8000/images/user.png', 100000, '087766665677'),
-(27, 'Irpan Maulana', 'irpan@gmail.com', '$2b$10$UiV7GpNW3nafcZFJ8Bw0dOVeqCHKxzzqkUoJKeF5TlIQyTLjsgFuy', '', 'http://34.205.65.98:8000/images/user.png', 100000, '087855555566'),
-(29, 'Nawirudin', 'nawi@gmail.com', '$2b$10$8FFqvxsZA.egLNk3uaRIj..hFTME6uAjxJMVGUHOtOVUouhfjkAb2', '632541', 'http://34.205.65.98:8000/images/user.png', 100000, '08783333339999'),
-(30, 'Wildan ', 'windan@gmail.com', '$2b$10$cp.c3iOYThDucWG2.GbyXuZ0cAh4kclGKMRu5/gSTIuKKVbZY6tfe', '632214', 'http://34.205.65.98:8000/images/user.png', 100000, '085221000888'),
-(31, 'Gana Ginting', 'gana@gmail.com', '$2b$10$1aa0/oC7KTFKubV1BW4KxeDVQ674NyD11v1pYizCh84zoItE4J4jq', '123456', 'http://34.205.65.98:8000/images/1602082367778-image.PNG', 300000, '08522400009876'),
-(32, 'Taufiq Widi', 'taufiq@gmail.com', '$2b$10$n6MHx7ehQR17Dw5t4sjpS.HSHnEbfKXmh3D8Ee3MJOfzEpzfLhB9u', '963258', 'http://34.205.65.98:8000/images/user.png', 100000, '0878444564578'),
-(33, 'Solehudin', 'soleh@gmail.com', '$2b$10$kySOfWiG9VUuQ7AVAFtDvOUTjv4cAjmoqmoAJHNevMRNjiaqrq0Ea', '632541', 'http://34.205.65.98:8000/images/user.png', 50000, ''),
-(35, 'Rizal Ramadhan', 'rizal@gmail.com', '$2b$10$sme2rgvNvuDoi0.oTjLNtu/xwSe..3Kvvqw4qMNJ8cOGtlE8TgeEe', '632541', 'http://34.205.65.98:8000/images/user.png', 0, '');
+(1, 'Yusup Junaedi', 'yusup.junaedi97@gmail.com', '$2b$10$aqvn7yjdI5mzn4FGIkjCUuRoH2uIHyYkr/e5GtslIf6V0bp1PbxzC', '632541', 'http://34.205.65.98:8000/images/1602082347863-image.jpg', 85000, '087823455567'),
+(23, 'zaki zull', 'zaki@gmail.com', '$2b$10$k14uRJOdQ8n1kTN6KNg94OffeTEDzO24NhgrIiCJwtJAn7aSCW4uC', '', 'http://34.205.65.98:8000/images/1601571285617-image.jpg', 100000, '0822888876744'),
+(24, 'Nendy Yusup', 'nendy@gmail.com', '$2b$10$U1xAoOzX/nzfAIYHQ0agMOFote6MqDLOEtxC3agGAHU1BoHKz323S', '', 'http://34.205.65.98:8000/images/1601576114648-image.jpg', 135000, '087722223455'),
+(25, 'Zaki ZUlikifli', 'zaki123@gmail.com', '$2b$10$RzILjX/4C8JLulSqmPNKe.joDEYgu5ytvgqPYaJLLNBjyy3xNZtVm', '', 'http://34.205.65.98:8000/images/1601606436577-image.jpg', 100000, '087820500512'),
+(26, 'amel', 'amel@gmail.com', '$2b$10$ACGqZjNrnUDSFbJ0LgrBl.ZW91gCNIi6.s2DgTH5uVByLUIluh1IW', '', 'http://34.205.65.98:8000/images/1601608076853-image.jpg', 100000, '087766665677'),
+(27, 'Irpan Maulana', 'irpan@gmail.com', '$2b$10$UiV7GpNW3nafcZFJ8Bw0dOVeqCHKxzzqkUoJKeF5TlIQyTLjsgFuy', '', 'http://34.205.65.98:8000/images/radu-florin-4_QFycgpC4c-unsplash.jpg', 100000, '087855555566'),
+(29, 'Nawirudin', 'nawi@gmail.com', '$2b$10$8FFqvxsZA.egLNk3uaRIj..hFTME6uAjxJMVGUHOtOVUouhfjkAb2', '632541', 'http://34.205.65.98:8000/images/logan-weaver-IVXoVHXqVCY-unsplash.jpg', 100000, '08783333339999'),
+(30, 'Wildan ', 'windan@gmail.com', '$2b$10$cp.c3iOYThDucWG2.GbyXuZ0cAh4kclGKMRu5/gSTIuKKVbZY6tfe', '632214', 'http://34.205.65.98:8000/images/allef-vinicius-3TSyMrZj5ik-unsplash.jpg', 100000, '085221000888'),
+(31, 'Gana Ginting', 'gana@gmail.com', '$2b$10$1aa0/oC7KTFKubV1BW4KxeDVQ674NyD11v1pYizCh84zoItE4J4jq', '123456', 'http://34.205.65.98:8000/images/engin-akyurt-_YR7sG21yGE-unsplash.jpg', 300000, '08522400009876'),
+(32, 'Taufiq Widi', 'taufiq@gmail.com', '$2b$10$n6MHx7ehQR17Dw5t4sjpS.HSHnEbfKXmh3D8Ee3MJOfzEpzfLhB9u', '963258', 'http://34.205.65.98:8000/images/emir-saldierna-oGcAzDcAXWU-unsplash.jpg', 100000, '0878444564578'),
+(33, 'Solehudin', 'soleh@gmail.com', '$2b$10$kySOfWiG9VUuQ7AVAFtDvOUTjv4cAjmoqmoAJHNevMRNjiaqrq0Ea', '632541', 'http://34.205.65.98:8000/images/ludovic-migneault-4uj3iZ5m084-unsplash.jpg', 50000, ''),
+(35, 'Rizal Ramadhan', 'rizal@gmail.com', '$2b$10$sme2rgvNvuDoi0.oTjLNtu/xwSe..3Kvvqw4qMNJ8cOGtlE8TgeEe', '632541', 'http://34.205.65.98:8000/images/albert-dera-ILip77SbmOE-unsplash.jpg', 0, '');
 
 --
 -- Indexes for dumped tables
@@ -197,7 +199,7 @@ ALTER TABLE `data_user`
 -- AUTO_INCREMENT for table `history`
 --
 ALTER TABLE `history`
-  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
 -- AUTO_INCREMENT for table `users`
